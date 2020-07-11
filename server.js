a2da5382f6f68aeaf081bffc967f3004992449e4
EthanChenYen-Peng <ypcethan@gmail.com>
2020-07-11 17:03:07 +0800

    adding database connection

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+const path = require('path');
 const express = require('express');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const connectDB = require('./utils/db');
 
 const app = express();
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: path.resolve(__dirname, 'config/dev.env') });
+  app.use(logger('dev'));
+}
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.json('Hello');
 });
-const start = () => {
+const start = async () => {
+  await connectDB();
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
   });
